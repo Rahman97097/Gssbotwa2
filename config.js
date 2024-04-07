844875d37f94260fc6f04fa6e72dd2e485f5f4fe
Rahman ullah <120171162+Rahman97097@users.noreply.github.com>
2024-04-07 22:33:48 +0500

    Update config.js

--- config.js
+++ config.js
@@ -7,11 +7,11 @@ global.link = 'https://whatsapp.com/channel/0029VaWJMi3GehEE9e1YsI1S'
 global.linkGroup = 'https://chat.whatsapp.com/E3PWxdvLc7ZCp1ExOCkEGp'
 
 global.apikey = 'bf2d2cf29b3edc604b447983';
-global.ownername = "𝑆𝛪𝐷 𝐵𝛨𝛥𝛪";
-global.owner = ['917050906659'];
-global.premium = ['917050906659'];
-global.packname = "𝚜𝚝𝚒𝚌𝚔𝚎𝚛 𝚋𝚢";
-global.author = "𝐆𝐒𝐒 𝚩𝚯𝚻𝐖𝚫";
+global.ownername = "☠️мґ.ʊηкη◎ẘη☠️";
+global.owner = ['923015954782'];
+global.premium = ['923015954782'];
+global.packname = "☛мα∂є ву мґ.ʊηкη◎ẘη☠️";
+global.author = "𝐷𝛥𝑅𝛯𝛫 𝛻𝛪𝐿 𝛭𝐷";
 global.sessionName = 'session';
 global.prefa = [".", "!", "/", "?"];
 global.typemenu = "v1"
